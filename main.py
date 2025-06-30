695a340a4b9997e93f40cb23c0ae050d311e99f8
mrtmexx <marat.nurmukhametov@setinbox.com>
2025-06-30 21:45:24 +0300

    Version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -11,6 +12,10 @@ def hello():
 @app.route("/test")
 def test():
     return "Test Page!"
+
+@app.route("/version")
+def version():
+    return os.environ.get('VERSION', 'UNDEFINED')
 
 if __name__ == '__main__':
     app.run(debug=True)
