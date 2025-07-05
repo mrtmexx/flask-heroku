9bd34e2a474b46ccb8cc4eed7ac45059d72ba74d
mrtmexx <marat.nurmukhametov@setinbox.com>
2025-07-05 14:53:28 +0300

    Add logging

--- main.py
+++ main.py
@@ -2,11 +2,21 @@
 # coding=utf-8
 
 import os
+import logging
 from flask import Flask
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    logger.info("Доступ к главной странице /")
     return "Hello World!"
 
 @app.route("/test")
